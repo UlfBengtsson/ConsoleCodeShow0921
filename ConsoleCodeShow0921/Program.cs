cf211c9b14be89b667da47d8ee3ec8bd28585fe6
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-09-21 09:59:43 +0200

    Add project files.

---
+++ ConsoleCodeShow0921/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleCodeShow0921
+{
+    class Program
+    {
+
+        /*
+
+        */
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            Console.WriteLine("Check: " + CheckForLetter(stringBuilder, 'd'));
+
+        }// end main
+
+        static bool CheckForLetter(StringBuilder stringBuilder, char letter)
+        {
+            string text = stringBuilder.ToString();
+
+            if (String.IsNullOrEmpty(text))
+            {
+
+                if (text.IndexOf(letter) == -1)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+
+    }// end program
+}// end namespace
